c3ce187ae9a359d53bc9bf36aa98bcd3a087279f
Al-Fathir <118950136+Alfthrpy@users.noreply.github.com>
2025-06-11 00:08:26 +0700

    feat: Remove unused modules and implement new structure for data extraction and uploading

---
+++ Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Beranda") # Opsional: Mengatur judul tab browser
+
+st.title("Selamat Datang di SIDAMA (Sistem Data Mahasiswa)")
--- pages/Extract.py
+++ pages/Extract.py
@@ -1,10 +1,9 @@
 import streamlit as st
 from supabase import create_client, Client
-from excel_uploader import display_excel_uploader
-from api_extractor import display_api_extractor
+from utils.excel_uploader import display_excel_uploader
+from utils.api_extractor import display_api_extractor
 
 st.set_page_config(page_title="SIDAMA: Sistem Integrasi Data Mahasiswa", layout="wide")
-
 @st.cache_resource
 def init_supabase_connection():
     """Membuat dan mengembalikan koneksi Supabase."""
@@ -17,7 +16,7 @@ def init_supabase_connection():
 
 supabase = init_supabase_connection()
 
-st.title("SIDAMA: Sistem Integrasi Data Mahasiswa")
+st.title("SIDAMA Extractor")
 st.markdown("Pilih metode ekstraksi data yang Anda inginkan di bawah ini.")
 
 if not supabase:
